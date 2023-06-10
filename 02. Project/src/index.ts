3f2a6f96db7b174c38337ee4f9ec5f9ac397054e
Ignacio Oliu <oliuignacio@gmail.com>
2023-06-10 16:42:20 +0200

    add: creating a jwt

--- 02. Project/src/index.ts
+++ 02. Project/src/index.ts
@@ -1,4 +1,7 @@
-import app from './server';
+import * as dotenv from 'dotenv'
+dotenv.config() // this will look dotenv file and load it on environment and now you can access through process.env
+
+import app from './server'
 
 app.listen(3001, () => {
   console.log("hello on http://localhost:3001");
---
+++ 02. Project/src/modules/auth.ts
@@ -0,0 +1,11 @@
+import jwt from 'jsonwebtoken'
+
+export const createJWT = (user) => {
+  const token = jwt.sign({
+    id: user.id, 
+    username: user.username,
+  }, 
+  process.env.JWT_SECRET)
+
+  return token
+}
