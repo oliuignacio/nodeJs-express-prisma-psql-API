5d2c31682880558fb5e37d519aa9fb768fde59ee
Ignacio Oliu <oliuignacio@gmail.com>
2023-06-11 23:09:17 +0200

    add: validating bearer token

--- 02. Project/src/modules/auth.ts
+++ 02. Project/src/modules/auth.ts
@@ -10,7 +10,7 @@ export const createJWT = (user) => {
   return token
 }
 
-export const protect = (req, res) => {
+export const protect = (req, res, next) => {
   //to authenticate we are setting the rule of passsing headers auth
   const bearer = req.headers.authorization
   // bearer token uses the word bearer before the token
@@ -21,4 +21,22 @@ export const protect = (req, res) => {
     return
   }
 
+  const [, token] = bearer.split(' ')
+
+  if (!token) {
+    res.status(401) // code for no authorization
+    res.json({message: 'not valid token'})
+    return
+  }
+
+  try {
+    const user = jwt.verify(token, process.env.JWT_SECRET)
+    req.user = user
+    next()
+  } catch (e) {
+    console.error(e)
+    res.status(401)
+    res.json({message: 'not valid token'})
+    return
+  }
 }
